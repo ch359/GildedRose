891f290d63c9e65c892726b2e842eebcfbd0f8da
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-04-06 17:27:04 +0100

    Eliminate legacy conditionals for backstage passes

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -53,36 +53,16 @@ class GildedRose
         next
       end
 
-      if item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          item.quality = item.quality - 1
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
+      if item.quality > 0
+        item.quality = item.quality - 1
       end
+
       item.sell_in = item.sell_in - 1
       if item.sell_in < 0
         if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              item.quality = item.quality - 1
-            end
-          else
-            item.quality = item.quality - item.quality
+
+          if item.quality > 0
+            item.quality = item.quality - 1
           end
         else
           if item.quality < 50
