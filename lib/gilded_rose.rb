eb9c5b5e79708f61f717735cd71dbe1c89f4de31
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-04-06 16:36:12 +0100

    Eliminate all if statements checking for legendaries

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -18,11 +18,10 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
+      next if legendary?(item)
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
+          item.quality = item.quality - 1
         end
       else
         if item.quality < 50
@@ -41,16 +40,12 @@ class GildedRose
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
+      item.sell_in = item.sell_in - 1
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
+              item.quality = item.quality - 1
             end
           else
             item.quality = item.quality - item.quality
